5b6bbd7a601e690925125c7ff63a22b13ae5c8eb
btech240 <brenttlong@gmail.com>
2024-01-08 17:35:48 -0500

    Added comments for needed code sections and configured screen

---
+++ main.py
@@ -0,0 +1,33 @@
+# Import needed modules
+from turtle import Screen, Turtle
+
+# Create screen and configure
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+
+# Create snake body
+
+
+# Move snake
+
+
+# Create snake food
+
+
+# Detect collision with food
+
+
+# Create a scoreboard
+
+
+# Detect collision with wall
+
+
+# Detect collision with tail
+
+
+# Prevent exit until click
+screen.exitonclick()
